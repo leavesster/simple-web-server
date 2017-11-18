4705d07c5ddfa6754244c2e0ce423b5741aa7a6d
leavesster <11785335+leavesster@users.noreply.github.com>
2017-11-18 12:50:59 +0800

    support uploads

--- server.py
+++ server.py
@@ -115,6 +115,42 @@ def jpg():
 def log_request():
     app.logger.info(time.asctime(time.localtime(time.time())) + ' ' + request.url)
 
+from werkzeug import secure_filename
+import os
+from flask import url_for
+
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'png', 'jpg', 'jepeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    """filename:string"""
+    return '.' in filename and filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    """ upload file """
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+from flask import send_from_directory
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 if __name__ == '__main__':
     manager.add_command("runserver", Server(use_debugger=True, host='0.0.0.0', port=5000))
     manager.run()
